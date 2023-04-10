5255ca4348f4077d1ef5eb483538e6fb29db4ae6
Iryna-Smirnova <irina_smirnova48@ukr.net>
2023-04-10 14:56:46 +0300

    hw07 task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+  
+  const galleryContainer = document.querySelector('.gallery');
+  
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+  
+  function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    }).join('');
+  }
 
-console.log(galleryItems);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
+  
+  
